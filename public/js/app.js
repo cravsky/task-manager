20353fe88512c6b1e1fc7660302ff9063fe4df84
cravsky <cravsky@gmail.com>
2022-08-18 09:24:53 +0200

    Created user interface for creating new users

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector('form')
+
+function createUserRequest(e) {
+    const formData = new FormData(e.target)
+    const newUser = JSON.stringify(Object.fromEntries(formData.entries()))
+    return newUser
+}
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const newUser = createUserRequest(e)
+
+    fetch("/users", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        body: newUser
+    }).then(res => {
+        if (res.error) {
+            console.log('Something went wrong')
+        } else {
+            console.log("Request complete! response:", res);
+        }
+    });
+
+})
--- src/index.js
+++ src/index.js
@@ -2,22 +2,42 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const auth = require('./middleware/auth')
-
+const hbs = require('hbs')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT
 
-app.use(express.json())
-app.get('/', (req, res) => {
-    try {
-        res.status(200).send('Everything is fine')
-    } catch (e) {
-        res.status(400).send()
-    }
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('login', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
+app.use(express.json())
+// app.get('/', (req, res) => {
+//     try {
+//         res.status(200).send('Everything is fine')
+//     } catch (e) {
+//         res.status(400).send()
+//     }
+
+// })
 
 app.use(userRouter)
 app.use(taskRouter)
